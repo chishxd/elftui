ae94699935bb4d76d6f8b88e7e8e88c1a858ab12
Chinmay Shet <chishxd@tutamail.com>
2026-07-23 03:15:06 +0530

    feat: Create responsive panes
    This is the basic skeleton of the project, the sidebar and info
    Currently the TUI looks fine in VSCode terminal and regular terminal,
    IDK why it is not working in Neovim terminal, like the top is kinda
    shaved, but htop, which is also written in ncurses looks fine, I'll
    figure it out

--- main.c
+++ main.c
@@ -1,38 +1,69 @@
 #include <curses.h>
 
-int main() {
+WINDOW *left_pane = NULL;
+WINDOW *right_pane = NULL;
+
+void create_panes() {
   int max_y, max_x;
-
-  initscr();
-  cbreak();
-  curs_set(FALSE);
-  noecho();
-
   getmaxyx(stdscr, max_y, max_x);
 
+  if (left_pane) delwin(left_pane);
+  if (right_pane) delwin(right_pane);
+
   int height = max_y;
-  int width = max_x / 2;
+  int lwidth = max_x / 4;
+
+  if (lwidth < 18) lwidth = 18;
+
+  if (lwidth > max_x - 10) lwidth = max_x - 10;
+
   int left_start = 0;
+  int rwidth = max_x - lwidth;
+  int right_start = lwidth;
 
-  int right_start = width;
-
-  WINDOW *left_pane = newwin(height, width, 0, left_start);
-  WINDOW *right_pane = newwin(height, width, 0, right_start);
+  left_pane = newwin(height, lwidth, 0, left_start);
+  right_pane = newwin(height, rwidth, 0, right_start);
 
   box(left_pane, 0, 0);
   box(right_pane, 0, 0);
 
   mvwprintw(left_pane, 1, 2, "Left Pane");
   mvwprintw(right_pane, 1, 2, "Right Pane");
+}
 
-  refresh();
-  wrefresh(left_pane);
-  wrefresh(right_pane);
+int main() {
 
-  getch();
+  initscr();
+  cbreak();
+  noecho();
+  keypad(stdscr, TRUE);
+  curs_set(FALSE);
 
-  delwin(left_pane);
-  delwin(right_pane);
+  create_panes();
+    
+    refresh();
+    wrefresh(left_pane);
+    wrefresh(right_pane);
+
+  int ch;
+
+  while ((ch = getch()) != 'q') {
+    switch (ch) {
+    case KEY_RESIZE:
+      clear();
+      refresh();
+
+      create_panes();
+      break;
+    default:
+      break;
+    }
+    wrefresh(left_pane);
+    wrefresh(right_pane);
+  }
+
+  if (left_pane) delwin(left_pane);
+  if (right_pane) delwin(right_pane);
   endwin();
 
   return 0;
