7662de263d04cca546a83431d4fcc52cb2471870
Chinmay Shet <chishxd@tutamail.com>
2026-07-23 02:12:28 +0530

    create proper panes for TUI

--- main.c
+++ main.c
@@ -1,13 +1,39 @@
 #include <curses.h>
 
+int main() {
+  int max_y, max_x;
 
-int main(){
-    initscr();
-    printw("Hello!");
-    refresh();
-    getch();
-    endwin();
-    
-    return 0;
+  initscr();
+  cbreak();
+  curs_set(FALSE);
+  noecho();
+
+  getmaxyx(stdscr, max_y, max_x);
+
+  int height = max_y;
+  int width = max_x / 2;
+  int left_start = 0;
+
+  int right_start = width;
+
+  WINDOW *left_pane = newwin(height, width, 0, left_start);
+  WINDOW *right_pane = newwin(height, width, 0, right_start);
+
+  box(left_pane, 0, 0);
+  box(right_pane, 0, 0);
+
+  mvwprintw(left_pane, 1, 2, "Left Pane");
+  mvwprintw(right_pane, 1, 2, "Right Pane");
+
+  refresh();
+  wrefresh(left_pane);
+  wrefresh(right_pane);
+
+  getch();
+
+  delwin(left_pane);
+  delwin(right_pane);
+  endwin();
+
+  return 0;
 }
-
