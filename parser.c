d6353e7c516809a4118f91f005705bd358f96f48
Chinmay Shet <chishxd@tutamail.com>
2026-07-25 00:52:25 +0530

    feat: Get CLASS of binary

--- parser.c
+++ parser.c
@@ -12,9 +12,9 @@ int main() {
     return 1;
   }
 
-  uint8_t buffer[4];
+  uint8_t buffer[16];
 
-  if (read(file, buffer, 4) != 4) {
+  if (read(file, buffer, 16) != 16) {
     perror("Error reading first 4 bytes");
     close(file);
     return 1;
@@ -23,6 +23,20 @@ int main() {
   if (buffer[0] == 0x7F && buffer[1] == 'E' && buffer[2] == 'L' &&
       buffer[3] == 'F') {
     printf("Valid ELF file\n");
+
+    switch (buffer[EI_CLASS]) {
+      case ELFCLASS32:
+        printf("32 bit object\n");
+        break;
+      case ELFCLASS64:
+        printf("64 bit object\n");
+        break;
+      case ELFCLASSNONE:
+        printf("Invaid object\n");
+        break;
+      default:
+        break;
+    }
   } else {
     printf("Invalid ELF file!\n");
   }
